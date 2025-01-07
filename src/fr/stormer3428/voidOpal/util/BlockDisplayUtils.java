c64e343ae66915044f97c532bcd4f824032527a4
PASTOR Charles <azertyuiopcharles@gmail.com>
2025-01-07 09:34:58 +0100

    Update BlockDisplayUtils.java

--- src/fr/stormer3428/voidOpal/util/BlockDisplayUtils.java
+++ src/fr/stormer3428/voidOpal/util/BlockDisplayUtils.java
@@ -9,53 +9,59 @@ import org.joml.Matrix4d;
 import org.joml.Matrix4f;
 
 public class BlockDisplayUtils {
-	
-	public static BlockDisplay createBlockDisplayLine(Location a, Location b, Material material, double width) {
+
+	public static BlockDisplay createBlockDisplayLine(Location a, Location b, Material material, double width) {return createBlockDisplayLine(a, b, material, width, 0);}
+	public static BlockDisplay createBlockDisplayLine(Location a, Location b, Material material, double width, double rotation) {return createBlockDisplayLine(a, b, material, width, rotation, false);}
+	public static BlockDisplay createBlockDisplayLine(Location a, Location b, Material material, double width, double rotation, boolean containPoints) {
 		if(a.getWorld() != b.getWorld()) return null;
 
 		BlockDisplay display = a.getWorld().spawn(a, BlockDisplay.class);
-		Matrix4f m1 = createLocationlinkingMatrix4f(a, b, width);
+		Matrix4d m1 = createLocationlinkingMatrix4d(a, b, width, rotation, containPoints);
 
-		display.setTransformationMatrix(m1);
+		display.setTransformationMatrix(new Matrix4f(m1));
 		display.setBlock(material.createBlockData());
 
 		return display;
 	}
-	
-	public static Matrix4f createLocationlinkingMatrix4f(Location a, Location b, double width) {return createLocationlinkingMatrix4f(a, b, width, false);}
-	public static Matrix4f createLocationlinkingMatrix4f(Location a, Location b, double width, boolean containPoints) {
+
+	public static Matrix4d createLocationlinkingMatrix4d(Location a, Location b, double width) {return createLocationlinkingMatrix4d(a, b, width, 0);}
+	public static Matrix4d createLocationlinkingMatrix4d(Location a, Location b, double width, double rotation) {return createLocationlinkingMatrix4d(a, b, width, rotation, false);}
+	public static Matrix4d createLocationlinkingMatrix4d(Location a, Location b, double width, double rotation, boolean containPoints) {
 		if(a.getWorld() != b.getWorld()) return null;
 
 		double dist = a.distance(b);
-		Location temp = a.clone().setDirection(b.toVector().subtract(a.toVector()).normalize());
+		Vector dir = b.clone().subtract(a).toVector().normalize();
+		Location temp = a.clone().setDirection(dir);
 		Matrix4d m1 = new Matrix4d();
-
-		m1.mul(getMatrix4fForRotationAlongAxis(new Vector(0,-1,0), (float) Math.toRadians(temp.getYaw())));
-		m1.mul(getMatrix4fForRotationAlongAxis(new Vector(1,0,0), (float) Math.toRadians(temp.getPitch())));
-		m1.translate(-width/2, -width/2, containPoints ? -width/2 : 0);
-		m1.scale(width, width, dist);
 		
-		return new Matrix4f(m1);
+		m1.mul(getMatrix4dForRotationAlongAxis(new Vector(0,-1,0), (float) Math.toRadians(temp.getYaw())));
+		m1.mul(getMatrix4dForRotationAlongAxis(new Vector(1,0,0), (float) Math.toRadians(temp.getPitch())));
+		m1.mul(getMatrix4dForRotationAlongAxis(new Vector(0,0,1), (float) rotation));
+		
+		m1.translate(-width/2, -width/2, containPoints ? -width/2 : 0);
+		m1.scale(width, width, containPoints ? dist + width : dist);
+
+		return new Matrix4d(m1);
 	}
 
-	public static Matrix4f createOrientatedCubeMatrix4f(Location loc, Vector dir, double size) {return createOrientatedCubeMatrix4f(loc, dir, size, -size/2, -size/2, -size/2);}
-	public static Matrix4f createOrientatedCubeMatrix4f(Location loc, Vector dir, double size, double translateX, double translateY, double translateZ) {
+	public static Matrix4d createOrientatedCubeMatrix4d(Location loc, Vector dir, double size) {return createOrientatedCubeMatrix4d(loc, dir, size, -size/2, -size/2, -size/2);}
+	public static Matrix4d createOrientatedCubeMatrix4d(Location loc, Vector dir, double size, double translateX, double translateY, double translateZ) {
 		Location temp = loc.clone().setDirection(dir);
 		Matrix4d m1 = new Matrix4d();
 
-		m1.mul(getMatrix4fForRotationAlongAxis(new Vector(0,-1,0), (float) Math.toRadians(temp.getYaw())));
-		m1.mul(getMatrix4fForRotationAlongAxis(new Vector(1,0,0), (float) Math.toRadians(temp.getPitch())));
+		m1.mul(getMatrix4dForRotationAlongAxis(new Vector(0,-1,0), (float) Math.toRadians(temp.getYaw())));
+		m1.mul(getMatrix4dForRotationAlongAxis(new Vector(1,0,0), (float) Math.toRadians(temp.getPitch())));
 		m1.translate(translateX,translateY,translateZ);
 		m1.scale(size, size, size);
-		return new Matrix4f(m1);
+		return new Matrix4d(m1);
 	}
-	
+
 	public static Location neutralize(Location loc) {
 		return loc.toVector().toLocation(loc.getWorld());
 	}
-	
-	public static Matrix4f getMatrix4fForRotationAlongAxis(Vector axis, float angle) {
-		Matrix4f m1 = new Matrix4f();
+
+	public static Matrix4d getMatrix4dForRotationAlongAxis(Vector axis, float angle) {
+		Matrix4d m1 = new Matrix4d();
 		new AxisAngle4f(angle, (float) axis.getX(), (float) axis.getY(), (float) axis.getZ()).get(m1);
 		return m1;
 	}
