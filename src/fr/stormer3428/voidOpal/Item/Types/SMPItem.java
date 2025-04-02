46f16de4420a1a5cb9d191e2ac2e08c17d9da183
PASTOR Charles <azertyuiopcharles@gmail.com>
2025-04-02 20:53:01 +0200

    Turns out a default value of 0 is what equates to a vanilla item

--- src/fr/stormer3428/voidOpal/Item/Types/SMPItem.java
+++ src/fr/stormer3428/voidOpal/Item/Types/SMPItem.java
@@ -34,7 +34,7 @@ public class SMPItem implements OMCItem {
 
 	private Material material = Material.ENCHANTED_BOOK;
 	private String displayName = null;
-	private int CMD = -1;
+	private int CMD = 0;
 	private String registryName = getClass().getSimpleName();
 	private final ArrayList<String> lore = new ArrayList<>();
 	private final ArrayList<ItemFlag> itemFlags = new ArrayList<>();
@@ -169,7 +169,7 @@ public class SMPItem implements OMCItem {
 	public boolean equals(ItemStack other) {
 		if(other == null) return false;
 		if(other.getType() != getMaterial()) return false;
-		ItemMeta meta= other.getItemMeta();
+		ItemMeta meta = other.getItemMeta();
 		if(meta == null) return false;
 		if(!meta.hasCustomModelData()) return CMD==-1;
 		if(meta.getCustomModelData() != getCMD()) return false;
