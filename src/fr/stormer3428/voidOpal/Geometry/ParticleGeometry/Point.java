81993ccb8e98c53b583e54d1be5c3b6c5b28eb4b
stormer3428 <azertyuiopcharles@gmail.com>
2025-06-05 14:44:45 +0200

    Update Point.java

--- src/fr/stormer3428/voidOpal/Geometry/ParticleGeometry/Point.java
+++ src/fr/stormer3428/voidOpal/Geometry/ParticleGeometry/Point.java
@@ -28,27 +28,27 @@ public class Point implements Drawable{
 	}
 
 	public int getParticleAmount() {return particleAmount;}
+	public float getParticleSpeed() {return particleSpeed;}
 	public float getParticleSpreadX() {return particleSpreadX;}
 	public float getParticleSpreadY() {return particleSpreadY;}
 	public float getParticleSpreadZ() {return particleSpreadZ;}
-	public float getParticleSpeed() {return particleSpeed;}
-	public Point location(Vector location) {this.location = location;return this;}
-	public Point drawChance(double drawChance) {this.drawChance = drawChance;return this;}
-	public Point particleAmount(int particleAmount) {this.particleAmount = particleAmount;return this;}
+	public Point location(Vector location) {this.location = location; return this;}
+	public Point drawChance(double drawChance) {this.drawChance = drawChance; return this;}
+	public Point particleSpeed(float particleSpeed) {this.particleSpeed = particleSpeed; return this;}
+	public Point particleAmount(int particleAmount) {this.particleAmount = particleAmount; return this;}
 	public Point particleSpread(float particleSpread) {particleSpreadX(particleSpread); particleSpreadY(particleSpread); particleSpreadZ(particleSpread); return this;}
-	public Point particleSpreadX(float particleSpreadX) {this.particleSpreadX = particleSpreadX;return this;}
-	public Point particleSpreadY(float particleSpreadY) {this.particleSpreadY = particleSpreadY;return this;}
-	public Point particleSpreadZ(float particleSpreadZ) {this.particleSpreadZ = particleSpreadZ;return this;}
-	public Point particleSpeed(float particleSpeed) {this.particleSpeed = particleSpeed;return this;}
+	public Point particleSpreadX(float particleSpreadX) {this.particleSpreadX = particleSpreadX; return this;}
+	public Point particleSpreadY(float particleSpreadY) {this.particleSpreadY = particleSpreadY; return this;}
+	public Point particleSpreadZ(float particleSpreadZ) {this.particleSpreadZ = particleSpreadZ; return this;}
 	public double getDrawChance() { return drawChance; }
 	public Vector getLocation() {return location;}
-	@Override public Point rotateAroundAxis(Vector axis, double radians) {location.rotateAroundAxis(axis, radians);return this;}
-	@Override public Point rotateAroundX(double radians) {location.rotateAroundX(radians);return this;}
-	@Override public Point rotateAroundY(double radians) {location.rotateAroundY(radians);return this;}
-	@Override public Point rotateAroundZ(double radians) {location.rotateAroundZ(radians);return this;}
-	@Override public Point particle(Particle particle) {this.particle = particle;return this;}
-	@Override public Point particleData(Object particleData) {this.particleData = particleData;return this;}
-	@Override public Point forceRendering(boolean forceRender) {this.forceRender = forceRender;return this;}
+	@Override public Point particle(Particle particle) {this.particle = particle; return this;}
+	@Override public Point particleData(Object particleData) {this.particleData = particleData; return this;}
+	@Override public Point rotateAroundX(double radians) {location.rotateAroundX(radians); return this;}
+	@Override public Point rotateAroundY(double radians) {location.rotateAroundY(radians); return this;}
+	@Override public Point rotateAroundZ(double radians) {location.rotateAroundZ(radians); return this;}
+	@Override public Point forceRendering(boolean forceRender) {this.forceRender = forceRender; return this;}
+	@Override public Point rotateAroundAxis(Vector axis, double radians) {location.rotateAroundAxis(axis, radians); return this;}
 	@Override public boolean isForceRendering() {return forceRender;}
 	@Override public Particle getParticle() {return particle;}
 
