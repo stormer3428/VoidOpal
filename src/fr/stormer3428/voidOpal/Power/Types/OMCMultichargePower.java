d47a516cb5483e4d9c04f44d7e2a09cbad2c1a78
stormer3428 <azertyuiopcharles@gmail.com>
2026-02-03 11:27:24 +0100

    Create OMCMultichargePower.java
    There is now a new ⁨OMCPower⁩ type called ⁨OMCMultichargePower⁩.
    If functions similarily to ⁨OMCPower⁩ except it implements a new function ⁨getMaxCharges⁩ which defines the amount of charges a power has
    
    It allows for example, to have a power that can store up to 3 dashes, each of those recharge sequentially (think of it like overwatch tracer blink)
    
    ⁨onCooldown⁩ will display the player as being in-cooldown as long as the player is missing ANY of their charges
    there is a new hashmap called  ⁨ON_COOLDOWN_CHARGES⁩ which stores the amount of spent (not remaining) charges per player
    
    The only new bit of code on the dev ends ends up being this single line. The rest is handled behind closed doors

---
+++ src/fr/stormer3428/voidOpal/Power/Types/OMCMultichargePower.java
@@ -0,0 +1,64 @@
+package fr.stormer3428.voidOpal.Power.Types;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+import java.util.Map.Entry;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public abstract class OMCMultichargePower extends OMCPower{
+
+	private final HashMap<UUID, Integer> ON_COOLDOWN_CHARGES = new HashMap<>(); // inverted so that players without that power are simply not in the map
+
+	public OMCMultichargePower(String registryName) { super(registryName); }
+
+	public abstract int getMaxCharges();
+
+	@Override
+	public boolean tryCast(ItemStack it, Player p, Map<String, Object> metadata) {
+		if(!isEnabled())return false;
+		if(ON_COOLDOWN_CHARGES.computeIfAbsent(p.getUniqueId(), u->0) >= getMaxCharges()) return false;
+		empower(it, p, metadata);
+		return true;
+	}
+
+	@Override
+	public void putOnCooldown(UUID uuid, int abilityCooldown) {
+		int spentCharges = ON_COOLDOWN_CHARGES.computeIfAbsent(uuid, u->0);
+		ON_COOLDOWN_CHARGES.put(uuid, spentCharges + 1);
+		onCooldown.putIfAbsent(uuid, Math.max(getCooldown(uuid), abilityCooldown));
+	}
+
+	@Override
+	public void onTick(int ticker) {
+		Iterator<Entry<UUID, Integer>> iterator = onCooldown.entrySet().iterator();
+		while(iterator.hasNext()) {
+			Entry<UUID, Integer> entry = iterator.next();
+			UUID uuid = entry.getKey();
+			int cooldown = entry.getValue();
+			cooldown --;
+			if(cooldown > 0) {
+				entry.setValue(cooldown);
+				continue;
+			}
+
+			int spentCharges = ON_COOLDOWN_CHARGES.get(uuid); // a computeIfAbsent would be a failsafe, but there should never be a cooldown if theres no charges to refresh
+			spentCharges--;// cooldown is over, we reset the cooldown and decrement the charge count
+			if(spentCharges == 0) {// no more charges to fill up, we can remove him entirely 
+				iterator.remove(); 
+				ON_COOLDOWN_CHARGES.remove(uuid);
+			}
+			else{ // there are still spent charges to recharge, we reset the cooldown to refresh the next charge
+				ON_COOLDOWN_CHARGES.put(uuid, spentCharges);
+				entry.setValue(getCooldown());
+			}
+			Player p = Bukkit.getPlayer(uuid);
+			if(p == null) continue;
+			onCooldownEnd(p);
+		}
+	}
+}
