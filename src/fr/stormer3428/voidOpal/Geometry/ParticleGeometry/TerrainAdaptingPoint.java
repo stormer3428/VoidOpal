239cda21bf7cece4303d1c4a292799cf6ca38e8f
stormer3428 <azertyuiopcharles@gmail.com>
2026-02-11 18:07:45 +0100

    Update TerrainAdaptingPoint.java

--- src/fr/stormer3428/voidOpal/Geometry/ParticleGeometry/TerrainAdaptingPoint.java
+++ src/fr/stormer3428/voidOpal/Geometry/ParticleGeometry/TerrainAdaptingPoint.java
@@ -21,31 +21,36 @@ public class TerrainAdaptingPoint extends Point{
 		World world = location.getWorld();
 		Location particleLoc = location.clone().add(this.location.clone().multiply(scale));
 		particleLoc = displace(particleLoc);
-		if(particleLoc == null) return this;
-		
-		world.spawnParticle(particle, particleLoc, particleAmount, particleSpreadX, particleSpreadY, particleSpreadZ, particleSpeed, particleData, forceRender);
+		if(particleLoc != null) world.spawnParticle(particle, particleLoc, particleAmount, particleSpreadX, particleSpreadY, particleSpreadZ, particleSpeed, particleData, forceRender);
 		return this;
 	}
 	
 	public Location displace(Location location) {
 		World world = location.getWorld();
 
+		
 		Location raySource = location.clone();
 		double effectiveMaxDisplacement = maxDisplacement;
 
-		if(!location.getBlock().isPassable()) {
-			while(!raySource.getBlock().isPassable() && effectiveMaxDisplacement < maxDisplacement*2) {
+		if(!location.getBlock().isPassable()) { // We're in a block
+			while(!raySource.getBlock().isPassable() && effectiveMaxDisplacement < maxDisplacement*2) { // we move up until out of block, or out of range
 				effectiveMaxDisplacement += .1;
 				raySource = location.clone().add(displacementAxis.clone().multiply(-effectiveMaxDisplacement));
 			}
-			if(!raySource.getBlock().isPassable()) return null;
+			if(!raySource.getBlock().isPassable()) return null; //Out of range and still in block, fail
+			//in range and successfully moved out
 		}
 		
-		RayTraceResult rtr = world.rayTraceBlocks(raySource, displacementAxis, effectiveMaxDisplacement);
-		if(rtr != null) return rtr.getHitPosition().toLocation(world);
-		else if(passIfMiss) return null;
+		//Either we're in air, or have been moved into air
 		
-		return location;
+		RayTraceResult rtr = world.rayTraceBlocks(raySource, displacementAxis, effectiveMaxDisplacement);
+		if(rtr != null) { // we hit a block!
+			return rtr.getHitPosition().toLocation(world).add(displacementAxis.clone().multiply(-.1));
+		}
+		
+		//We are in air
+		
+		return passIfMiss ? null : location;
 	}
 
 	public TerrainAdaptingPoint passIfMiss() { passIfMiss = true; return this; }
